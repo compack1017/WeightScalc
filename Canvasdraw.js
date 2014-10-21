63e57b164adaa8f014e634dc03115ef326a87b07
compack1017 <compak1017@gmail.com>
2014-10-21 21:05:17 +0900

    体重計
    main

--- Canvasdraw.js
+++ Canvasdraw.js
@@ -1,1 +1,12 @@
+var canvas = document.getElementById('canvas');
 
+if(canvas.getContext){
+    var context = canvas.getContext('2d');
+    
+}else{
+    alert('Error!!');  
+};
+
+function scalc(){
+    
+}
